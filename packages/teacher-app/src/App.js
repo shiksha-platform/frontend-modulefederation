725360c628f49917a5ae17818e30de302febc622
Arun <arun.rajput@gmail.com>
2022-03-03 12:05:44 +0530

    teacher app theme fixes.dependency updatted

--- packages/teacher-app/src/App.js
+++ packages/teacher-app/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import { extendTheme, NativeBaseProvider } from "native-base";
 import { initReactI18next } from "react-i18next";
@@ -8,6 +9,7 @@ import Home from "./pages/Home";
 import init from "lang/init";
 
 import manifest from "./manifest.json";
+import { eventBus } from "@shiksha/common-lib";
 
 //TODO: separate out the theme related code from App
 i18n.use(initReactI18next).init(init);
@@ -17,29 +19,51 @@ const fontFamily =
   localStorage.getItem("lang") === "hi" ? "'Baloo 2'" : "Inter";
 const fontSize = localStorage.getItem("lang") === "hi" ? "20px" : "";
 
-function App() {
-  const token = sessionStorage.getItem("token");
-  if (!token) {
-    const LoginComponent = React.lazy(() => import("core/Login"));
+let red = {
+  50: "#fef2f2",
+  100: "#fde5e5",
+  150: "#fcd7d7",
+  200: "#fbcaca",
+  250: "#fabdbd",
+  300: "#f9b0b0",
+  350: "#f8a3a3",
+  400: "#f79595",
+  450: "#f68888",
+  500: "#f57b7b",
+  550: "#dd6f6f",
+  600: "#c46262",
+  650: "#ac5656",
+  700: "#934a4a",
+  750: "#7b3e3e",
+  800: "#623131",
+  850: "#492525",
+  900: "#311919",
+  950: "#180c0c",
+};
 
-    return (
-      <NativeBaseProvider>
-        <React.Suspense fallback="Loading ">
-          <LoginComponent />
-        </React.Suspense>
-      </NativeBaseProvider>
-    );
-  } else {
-    return (
-      <NativeBaseProvider>
-        <Home />
-      </NativeBaseProvider>
-    );
-  }
-}
+let green = {
+  50: "#e7f4e8",
+  100: "#cfe9d1",
+  150: "#b6debb",
+  200: "#9ed3a4",
+  250: "#86c98d",
+  300: "#6ebe76",
+  350: "#56b35f",
+  400: "#3da849",
+  450: "#259d32",
+  500: "#0d921b",
+  550: "#0c8318",
+  600: "#0a7516",
+  650: "#096613",
+  700: "#085810",
+  750: "#07490e",
+  800: "#053a0b",
+  850: "#042c08",
+  900: "#031d05",
+  950: "#010f03",
+};
 
-const theme = extendTheme(
-{
+const theme = extendTheme({
   fonts: {
     heading: fontFamily,
     body: fontFamily,
@@ -57,6 +81,14 @@ const theme = extendTheme(
       baseStyle: {
         maxW: maxWidth,
         alignSelf: "center",
+      },
+    },
+    Button: {
+      baseStyle: {
+        rounded: "lg",
+      },
+      defaultProps: {
+        colorScheme: "button",
       },
     },
   },
@@ -77,7 +109,9 @@ const theme = extendTheme(
     reportCard: {
       500: "#FFCAAC",
     },
+    present: green,
     presentCardBg: {
+      400: "#CEEED1",
       500: "#DFFDE2",
       600: "#cae3ce",
     },
@@ -91,6 +125,7 @@ const theme = extendTheme(
     presentCardCompareText: {
       500: "#FA8129",
     },
+    absent: red,
     absentCardBg: {
       500: "#FDE7E7",
       600: "#dfcbcb",
@@ -99,12 +134,11 @@ const theme = extendTheme(
       500: "#FFF6F6",
       600: "#dfcbcb",
     },
-    absentCardText: {
-      500: "#F57B7B",
-    },
+    absentCardText: red,
     absentCardCompareText: {
       500: "#FA8129",
     },
+    special_duty: { 500: "#06D6A0" },
     weekCardCompareBg: {
       500: "#FFF8F7",
     },
@@ -126,15 +160,13 @@ const theme = extendTheme(
       600: "#2BB639",
       500: "#2BB639",
     },
-    attendanceAbsent: {
-      600: "#F57B7B",
-      500: "#F57B7B",
-    },
+    attendanceAbsent: red,
     attendanceUnmarked: {
-      600: "#C4C4D4",
-      500: "#C4C4D4",
-      400: "#d3d3e5",
       100: "#F0F0F4",
+      300: "#B5B5C8",
+      400: "#d3d3e5",
+      500: "#C4C4D4",
+      600: "#C4C4D4",
     },
     timeTableCardOrange: {
       500: "#FFF7F5",
@@ -142,4 +174,57 @@ const theme = extendTheme(
   },
 });
 
+
+function App() {
+  const [token, setToken] = useState(sessionStorage.getItem("token"));
+
+  useEffect(() => {
+    const subscription = eventBus.subscribe("AUTH", (data, envelop)=>{
+      console.log(envelop);
+      if(data.eventType="LOGIN_SUCCESS"){
+        setToken(sessionStorage.getItem("token"))
+      }
+    });
+    return ()=>{
+      eventBus.unsubscribe(subscription);
+    }
+  }, [token]);
+  if (!token) {
+    const LoginComponent = React.lazy(() => import("core/Login"));
+    return (
+      <NativeBaseProvider theme={theme}>
+        <React.Suspense fallback="Loading ">
+          <LoginComponent />
+        </React.Suspense>
+      </NativeBaseProvider>
+    );
+  } else {
+    const MyClasses = React.lazy(() => import("core/MyClasses"));
+
+    return (
+      <NativeBaseProvider theme={theme}>
+        <React.Suspense fallback="Loading ">
+
+        <Router>
+        <Routes>
+          <Route
+            path="classes"
+            element={
+                 <MyClasses />
+            }
+          />
+          <Route
+            path="*"
+            element={
+                 <Home />
+            }
+          />
+        </Routes>
+      </Router>
+
+       </React.Suspense>
+      </NativeBaseProvider>
+    );
+  }
+}
 export default App;
--- packages/teacher-app/src/pages/Home.js
+++ packages/teacher-app/src/pages/Home.js
@@ -1,23 +1,73 @@
-
-import {Header} from "@shiksha/common-lib";
+import { Layout } from "@shiksha/common-lib";
 import { useTranslation } from "react-i18next";
 
-function Home (){
-    const { t } = useTranslation();
+import * as moment from "moment";
+function Home() {
+  const { t } = useTranslation();
 
-    return (
-        <>
-        <Header
-          title={t("MY_SCHOOL_APP")}
-          icon="sign-in-alt"
-          heading={t("LOGIN")}
-          _box={{ backgroundColor: "lightBlue.100" }}
-          _icon={{ color: "black" }}
-          _heading={{ color: "black" }}
-          _subHeading={{ color: "black" }}
-        />
-        Home ...
-        </>
-    )
+  return (
+    <>
+      <Layout
+        _header={{
+          title: t("HOME"),
+          icon: "Group",
+          subHeading: moment().format("hh:mm a"),
+          _subHeading: { fontWeight: 500, textTransform: "uppercase" },
+          avatar: true,
+        }}
+        _appBar={{languages:['en']}}
+        subHeader={t("THE_CLASSES_YOU_TAKE")}
+        _subHeader={{
+          bg: "classCard.500",
+          _text: {
+            fontSize: "16px",
+            fontWeight: "600",
+            textTransform: "inherit",
+          },
+        }}
+        _footer={{
+            menues: [
+                {
+                  "title": "HOME",
+                  "icon": "Home4LineIcon",
+                  "module": "Registry",
+                  "route": "/",
+                  "routeparameters": {}
+                },
+                {
+                  "title": "CLASSES",
+                  "icon": "TeamLineIcon",
+                  "module": "Registry",
+                  "route": "/classes",
+                  "routeparameters": {}
+                },
+                {
+                  "title": "SCHOOL",
+                  "icon": "GovernmentLineIcon",
+                  "module": "Registry",
+                  "route": "/",
+                  "routeparameters": {}
+                },
+                {
+                  "title": "MATERIALS",
+                  "icon": "BookOpenLineIcon",
+                  "module": "Registry",
+                  "route": "/",
+                  "routeparameters": {}
+                },
+                {
+                  "title": "CAREER",
+                  "icon": "UserLineIcon",
+                  "module": "Registry",
+                  "route": "/",
+                  "routeparameters": {}
+                }
+              ]
+        }}
+      >
+        <div>Home ...</div>
+      </Layout>
+    </>
+  );
 }
 export default Home;
