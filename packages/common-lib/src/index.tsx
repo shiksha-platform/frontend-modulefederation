4e94da061cb5b1066631256023d55daf8af83378
Arun Rajput <arunrajput@Aruns-MacBook-Pro-2.local>
2022-03-22 12:32:31 +0530

    fix: common-lib - redo i18n revert

--- packages/common-lib/src/index.tsx
+++ packages/common-lib/src/index.tsx
@@ -9,26 +9,19 @@ export const ExampleComponent = ({ text }: Props) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
 
-import Header from './components/layout/Header'
-import Footer from './components/layout/Footer'
-import Layout from './components/layout/Layout'
-import IconByName from './components/IconByName'
-import Widget from './components/Widget'
-import Collapsible from './components/Collapsible'
-import Menu from './components/Menu'
-import DEFAULT_THEME from './components/theme'
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Layout from './components/layout/Layout';
+import IconByName from './components/IconByName';
+import Widget from './components/Widget';
+import Collapsible from './components/Collapsible';
+import Menu from './components/Menu';
+import DEFAULT_THEME from './components/theme';
+import  initializeI18n from './services/i18n';
 
-export {
-  Header,
-  Footer,
-  Layout,
-  IconByName,
-  Widget,
-  Collapsible,
-  Menu,
-  DEFAULT_THEME
-}
+export {Header, Footer, Layout, IconByName, Widget,
+    Collapsible, Menu, DEFAULT_THEME, initializeI18n };
 
-export * from './services/Auth'
-export * from './services/RestClient'
-export * from './services/EventBus'
+export * from './services/Auth';
+export * from './services/RestClient';
+export * from './services/EventBus';
---
+++ packages/common-lib/src/services/i18n.ts
@@ -0,0 +1,53 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import Backend from 'i18next-http-backend';
+
+
+const languageDetector:any = {
+    type: 'languageDetector',
+    async: true, // If this is set to true, your detect function receives a callback function that you should call with your language, useful to retrieve your language stored in AsyncStorage for example
+    init: function(services:any, detectorOptions:any, i18nextOptions:any) {
+      /* use services and options */
+    },
+    detect: function(callback:any) { // You'll receive a callback if you passed async true
+      /* return detected language */
+      // callback('de'); if you used the async flag
+      let selectedLanguage = localStorage.getItem("lang");
+      return callback(selectedLanguage);
+    },
+    cacheUserLanguage: function(lng:any) {
+      /* cache language */
+    }
+}
+
+const initializeI18n = (namespaces:string[]) => {
+  console.log(namespaces);
+
+i18n
+  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+  // learn more: https://github.com/i18next/i18next-http-backend
+  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
+  .use(Backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(languageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    fallbackLng: 'en',
+    debug: true,   
+    ns: namespaces, 
+    fallbackNS:'translation',
+    backend:{
+      loadPath: '/locales/{{lng}}/{{ns}}.json' // http://module-base-url/locales/
+    },
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    }
+  });
+}
+
+export default initializeI18n;
