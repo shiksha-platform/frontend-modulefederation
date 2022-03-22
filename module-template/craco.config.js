cdcc9e5bb0ebf08051157473bc1fa331b6aa6356
Arun Rajput <arunrajput@Aruns-MacBook-Pro-2.local>
2022-03-22 18:34:19 +0530

    feat: module-template added

---
+++ module-template/craco.config.js
@@ -0,0 +1,16 @@
+const cracoModuleFederation = require("craco-module-federation");
+const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
+
+module.exports = {
+  devServer: {
+    port: 4001,
+  },
+  webpack: {
+    plugins: [new ExternalTemplateRemotesPlugin()],
+  },
+  plugins: [
+    {
+      plugin: cracoModuleFederation,
+    },
+  ],
+};
