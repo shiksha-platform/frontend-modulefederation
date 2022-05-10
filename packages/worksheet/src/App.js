ab638ba6ed710e7b189100da2cbc228066d10261
Shruti Agarwal <mail2shruti.ag@gmail.com>
2022-05-10 10:29:31 +0530

    feat: worksheet card
    Signed-off-by: Shruti Agarwal <mail2shruti.ag@gmail.com>

--- packages/worksheet/src/App.js
+++ packages/worksheet/src/App.js
@@ -4,6 +4,7 @@ import { extendTheme, NativeBaseProvider } from "native-base";
 import { DEFAULT_THEME } from "@shiksha/common-lib";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import QuestionBank from "pages/QuestionBank";
+import ViewWorksheet from "pages/ViewWorksheet";
 
 export default function App() {
   const theme = extendTheme(DEFAULT_THEME);
@@ -12,6 +13,7 @@ export default function App() {
       <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<QuestionBank />} />
+          <Route path="/viewworksheets" element={<ViewWorksheet />} />
         </Routes>
       </Router>
     </NativeBaseProvider>
