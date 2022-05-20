508f11f00668197926c94c6711fa9c9d3e6110eb
Neelesh2512 <neeleshsharma2512@gmail.com>
2022-05-21 03:20:52 +0530

    remove unwanted code

--- packages/core/src/App.js
+++ packages/core/src/App.js
@@ -7,8 +7,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AppShell, DEFAULT_THEME } from "@shiksha/common-lib";
 
 import { initializeI18n } from "@shiksha/common-lib";
-
-import {Helmet} from "react-helmet";
 initializeI18n(["translation", "core"]);
 
 const theme = extendTheme(DEFAULT_THEME);
@@ -16,12 +14,6 @@ const theme = extendTheme(DEFAULT_THEME);
 function App() {
   return (
     <NativeBaseProvider theme={theme}>
-    <Helmet>
-        <meta charSet="utf-8" />
-        <title>My Title</title>
-        <link rel="canonical" href="http://mysite.com/example" />
-        <meta name="description" content="Shiksha" />
-    </Helmet>
       <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="*" element={<Login />} />
