cd90f22d074a7bb2c9d216373320e0ebd30c0e47
Neelesh2512 <neeleshsharma2512@gmail.com>
2022-05-21 03:17:14 +0530

    favicon changed

--- packages/core/src/App.js
+++ packages/core/src/App.js
@@ -8,6 +8,7 @@ import { AppShell, DEFAULT_THEME } from "@shiksha/common-lib";
 
 import { initializeI18n } from "@shiksha/common-lib";
 
+import {Helmet} from "react-helmet";
 initializeI18n(["translation", "core"]);
 
 const theme = extendTheme(DEFAULT_THEME);
@@ -15,6 +16,12 @@ const theme = extendTheme(DEFAULT_THEME);
 function App() {
   return (
     <NativeBaseProvider theme={theme}>
+    <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Shiksha" />
+    </Helmet>
       <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="*" element={<Login />} />
