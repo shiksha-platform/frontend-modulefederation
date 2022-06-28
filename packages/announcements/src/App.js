c43eb0ba113135557b4a5bc07e9603857e8b1154
Hardik Sharma <hardik00051@gmail.com>
2022-06-28 19:55:50 +0530

    Feature:add announcement module

---
+++ packages/announcements/src/App.js
@@ -0,0 +1,32 @@
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import { extendTheme, NativeBaseProvider } from "native-base";
+import { DEFAULT_THEME, AppShell, initializeI18n } from "@shiksha/common-lib";
+import Announcements from "pages/Announcements";
+
+function App() {
+  const theme = extendTheme(DEFAULT_THEME);
+  initializeI18n(
+    ["announcements"],
+    `${process.env.PUBLIC_URL}/locales/{{lng}}/{{ns}}.json`
+  );
+  const routes = [
+    {
+      path: "announcements",
+      component: Announcements,
+    },
+    {
+      path: "*",
+      component: Announcements,
+    },
+  ];
+  const LoginComponent = React.lazy(() => import("core/Login"));
+
+  return (
+    <AppShell theme={theme} routes={routes} AuthComponent={LoginComponent} />
+  );
+}
+
+export default App;
