6efe8b228edcd8d370cbb3bc57bd30466a7c3c27
Nirmal <nirmalssbohra@gmail.com>
2022-07-21 13:39:14 +0530

    feat: written spot assessment

--- packages/assessment/src/components/SpotAssessment/SpotAssessmentResult.js
+++ packages/assessment/src/components/SpotAssessment/SpotAssessmentResult.js
@@ -20,6 +20,7 @@ import {
   Button,
   Actionsheet,
   Avatar,
+  Divider,
 } from "native-base";
 import { useTranslation } from "react-i18next";
 import moment from "moment";
@@ -41,6 +42,7 @@ const SpotAssessmentResult = ({ appName }) => {
   const [toDoNextModal, setToDoNextModal] = useState(false);
   const [similarTestModal, setSimilarTestModal] = useState(false);
   const [nextOption, setNextOption] = useState();
+  const score = localStorage.getItem("assessment-score");
 
   const handleNextOption = () => {
     if (nextOption === "similar") {
@@ -85,6 +87,189 @@ const SpotAssessmentResult = ({ appName }) => {
   useEffect(() => {
     _handleSpotAssessmentEnd();
   }, []);
+
+  if (score > 0) {
+    return (
+      <Layout isDisabledAppBar={true}>
+        <Loading
+          width={width}
+          height={height}
+          customComponent={
+            <VStack space="0" flex="1" width={width}>
+              <VStack bg={colors.scoreCardBg1} pb="100px" pt="32px">
+                <IconByName
+                  alignSelf="center"
+                  name="EmotionNormalLineIcon"
+                  color={colors.scoreCardIcon1}
+                  _icon={{ size: 100 }}
+                />
+                <Box alignItems="center">
+                  <H1 color={colors.scoreCardIcon1}>NOT BAD!</H1>
+                  <BodyLarge color={colors.scoreCardIcon1}>
+                    You got most of the answers right.
+                  </BodyLarge>
+                </Box>
+              </VStack>
+              <VStack space={50} bg="white">
+                <Box alignItems="center">
+                  <Box textAlign="center" marginTop="-40px">
+                    <VStack space={3}>
+                      <Box mx="auto">
+                        <Avatar
+                          size="80px"
+                          borderRadius="md"
+                          source={{
+                            uri: "https://via.placeholder.com/80x80.png",
+                          }}
+                        />
+                      </Box>
+                      <Box>
+                        <H2>Shah Rukh Khan</H2>
+                        <BodyMedium color={colors.gray}>
+                          Mr. Father’s Name
+                        </BodyMedium>
+                      </Box>
+                    </VStack>
+                  </Box>
+                </Box>
+                <Box>
+                  <VStack>
+                    <Box p="4" alignItems="center">
+                      <RoundedProgressBar
+                        values={[63, 28]}
+                        colors={[
+                          colors.scoreCardIcon1,
+                          colors.circleProgressBarcolor,
+                        ]}
+                        title={{ text: "63%", fontSize: "21px" }}
+                        legend={{ text: "Total Score", fontSize: "14px" }}
+                        cutout={"85%"}
+                        size="80px"
+                      />
+                      {/* <HStack justifyContent={"center"} alignItems="center">
+                      <IconByName name="StarFillIcon" p="0" color="#E78D12" />
+                      <IconByName name="StarFillIcon" p="0" color="#E78D12" />
+                      <IconByName name="StarFillIcon" p="0" color="#E78D12" />
+                      <IconByName
+                        name="StarHalfFillIcon"
+                        p="0"
+                        color="#E78D12"
+                      />
+                      <IconByName name="StarLineIcon" p="0" color="#E78D12" />
+                    </HStack> */}
+                    </Box>
+                  </VStack>
+                </Box>
+                <Box px="4">
+                  <Button
+                    colorScheme="button"
+                    _text={{
+                      color: colors.white,
+                    }}
+                    onPress={() => setToDoNextModal(true)}
+                  >
+                    {t("Return to teachers")}
+                  </Button>
+                </Box>
+              </VStack>
+            </VStack>
+          }
+        />
+        <Actionsheet
+          isOpen={toDoNextModal}
+          onClose={() => setToDoNextModal(false)}
+        >
+          <Actionsheet.Content alignItems={"left"} bg={colors.cardBg}>
+            <HStack justifyContent={"space-between"}>
+              <Stack p={5} pt={2} pb="15px">
+                <H2>{t("What would you like to do next?")}</H2>
+              </Stack>
+              <IconByName
+                name="CloseCircleLineIcon"
+                color={colors.cardCloseIcon}
+                onPress={(e) => setToDoNextModal(false)}
+              />
+            </HStack>
+          </Actionsheet.Content>
+          <Box w="100%" p={4} justifyContent="center" bg={colors.white}>
+            <Actionsheet.Item onPress={() => setNextOption("repeat")}>
+              <BodyLarge>Repeat test with another student</BodyLarge>
+            </Actionsheet.Item>
+            <Actionsheet.Item onPress={() => setNextOption("similar")}>
+              <BodyLarge>Give similar test to another student</BodyLarge>
+            </Actionsheet.Item>
+            <Actionsheet.Item onPress={() => setNextOption("end")}>
+              <BodyLarge>End Assessment</BodyLarge>
+            </Actionsheet.Item>
+            <Divider my={4}></Divider>
+
+            <Box p="4" pt="0">
+              <Button
+                colorScheme="button"
+                _text={{
+                  color: colors.white,
+                }}
+                // onPress={()=> setSelectedStudent()}
+                onPress={() => handleNextOption()}
+              >
+                {t("Continue")}
+              </Button>
+            </Box>
+          </Box>
+        </Actionsheet>
+
+        <Actionsheet
+          isOpen={similarTestModal}
+          onClose={() => setSimilarTestModal(false)}
+        >
+          <Actionsheet.Content alignItems={"left"} bg={colors.cardBg}>
+            <HStack justifyContent={"space-between"}>
+              <Stack p={5} pt={2} pb="15px">
+                <H2>{t("Give similar test to another student")}</H2>
+              </Stack>
+              <IconByName
+                name="CloseCircleLineIcon"
+                color={colors.cardCloseIcon}
+                onPress={(e) => setSimilarTestModal(false)}
+              />
+            </HStack>
+          </Actionsheet.Content>
+          <Box w="100%" p={4} justifyContent="center" bg={colors.white}>
+            <BodyMedium my={3}>
+              A similar test will consist of the same competencies with a
+              different set of questions.
+            </BodyMedium>
+            <BodyMedium my={3}>Are you sure you want to continue?</BodyMedium>
+            <Divider my={4}></Divider>
+
+            <Box>
+              <HStack justifyContent={"space-between"}>
+                <Button
+                  colorScheme="button"
+                  _text={{
+                    color: colors.white,
+                  }}
+                  // onPress={()=> setSelectedStudent()}
+                >
+                  {t("Choose  COMPETENCIES")}
+                </Button>
+
+                <Button
+                  colorScheme="button"
+                  _text={{
+                    color: colors.white,
+                  }}
+                  onPress={() => handleContinueWithSimilarQuestion()}
+                >
+                  {t("Yes, Continue")}
+                </Button>
+              </HStack>
+            </Box>
+          </Box>
+        </Actionsheet>
+      </Layout>
+    );
+  }
 
   return (
     <Layout isDisabledAppBar={true}>
--- packages/common-lib/src/services/assessmentRegistryService.js
+++ packages/common-lib/src/services/assessmentRegistryService.js
@@ -1,5 +1,25 @@
 // import { get, post } from './RestClient'
 import axios from 'axios'
+import manifest from '../manifest.json'
+import mapInterfaceData from './mapInterfaceData'
+const defaultAdapter = 'diksha'
+
+const interfaceData = {
+  id: 'groupId',
+  schoolId: 'schoolId',
+  type: 'type',
+  name: 'name',
+  section: 'section',
+  status: 'status',
+  image: 'image',
+  mergeParameterWithValue: {
+    title: 'name'
+  },
+  mergeParameterWithDefaultValue: {
+    icon: 'calendar',
+    route: '/classes/:id'
+  }
+}
 
 export async function get(url, headers = {}) {
   return await axios.get(url, {
@@ -72,6 +92,104 @@ const readQuestion = async (questionId) => {
       qType,
       questionId: identifier
     }
+  } else {
+    return []
+  }
+}
+
+export const getSubjectsList = async (params = {}, header = {}) => {
+  const headers = {
+    ...header,
+    Authorization: 'Bearer ' + localStorage.getItem('token')
+  }
+  const adapter = params.adapter ? params.adapter : defaultAdapter
+  const result = await get(
+    `${manifest.api_url}/question/${adapter}/subjectlist`,
+    params,
+    {
+      headers
+    }
+  )
+
+  if (result.data && result.data.data) {
+    return result.data.data.sort()
+  } else {
+    return []
+  }
+}
+
+export const getCompetenciesList = async (params = {}, header = {}) => {
+  const headers = {
+    ...header,
+    Authorization: 'Bearer ' + localStorage.getItem('token')
+  }
+  const adapter = params.adapter ? params.adapter : defaultAdapter
+  const result = await get(
+    `${manifest.api_url}/question/${adapter}/competencieslist`,
+    {
+      params,
+      headers
+    }
+  )
+
+  if (result.data && result.data.data) {
+    return result.data.data.sort()
+  } else {
+    return []
+  }
+}
+
+export const createUpdateAssessment = async (params = {}, header = {}) => {
+  const headers = {
+    ...header,
+    Authorization: 'Bearer ' + localStorage.getItem('token')
+  }
+  const result = await post(`${manifest.api_url}/trackassessment`, params, {
+    headers
+  })
+
+  if (result.data && result.data.data) {
+    return result.data.data
+  } else {
+    return {}
+  }
+}
+
+export const getAssessmentDetails = async (params = {}, header = {}) => {
+  const headers = {
+    ...header,
+    Authorization: 'Bearer ' + localStorage.getItem('token')
+  }
+  const result = await get(`${manifest.api_url}/trackassessment/${params}`, {
+    headers
+  })
+
+  if (result.data && result.data.data) {
+    return result.data.data
+  } else {
+    return {}
+  }
+}
+
+export const getAttendanceDetailsByClass = async (
+  groupId,
+  params = {},
+  header = {}
+) => {
+  const headers = {
+    ...header,
+    Authorization: 'Bearer ' + localStorage.getItem('token')
+  }
+  const result = await get(
+    `${manifest.api_url}/attendance/${groupId}/studentdetails`,
+    {
+      params,
+      headers
+    }
+  )
+
+  if (result.data && result.data.data) {
+    return result.data.data.sort()
   } else {
     return []
   }
