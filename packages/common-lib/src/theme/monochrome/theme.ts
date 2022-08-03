d5483f2cb45f70cdb0ca8b210c47f75bcf24aac9
sagar takle <sagarkoshti1990@gmail.com>
2022-08-03 19:28:43 +0530

    fix:color them at native base config

--- packages/common-lib/src/theme/monochrome/theme.ts
+++ packages/common-lib/src/theme/monochrome/theme.ts
@@ -1,9 +1,15 @@
 const maxWidth = '1080'
 const fontFamily = localStorage.getItem('lang') === 'hi' ? "'Baloo 2'" : 'Inter'
 const fontSize = localStorage.getItem('lang') === 'hi' ? '20px' : ''
+import colorTheme from './colorTheme'
 
 const theme = {
   colors: {
+    ...colorTheme,
+    attendance: colorTheme,
+    student: colorTheme,
+    class: colorTheme,
+    worksheet: colorTheme,
     widgetColor: {
       400: '#7F9DAC',
       500: '#DDD8F3',
@@ -32,38 +38,7 @@ const theme = {
       7600: '#1e1c63',
       800: '#12113c',
       900: '#060614'
-    },
-    worksheet: {
-      primaryLight: '#E0DFF6',
-      primaryDark: '#6461D2',
-      primaryNormal: '#E0DFF6',
-      primaryLight1: '#FFF8F7',
-      cardTitle: '#383739',
-      cardBg: '#F9CCE4',
-      cardBgLight: '#FEF1F9',
-      cardCloseIcon: '#7F9DAC',
-      grayLight: '#d4d4d8',
-      successAlertText: '#0D921B',
-      successAlert: '#B9FBC0',
-      warningAlert: '#FFE5B3',
-      viewNotificationNormal: '#FDDFD8',
-      viewNotificationDark: '#feefeb',
-      gray: '#6b7280',
-      green: '#0d911c',
-      darkGreen: '#5B7E5F',
-      lightGray1: '#9ca3af',
-      lightGray2: '#d1d5db',
-      lightGray3: '#e5e7eb',
-      lightGray4: '#f3f4f6',
-      lightGray5: '#f4f4f5',
-      worksheetCardBg: '#F9CCE4',
-      worksheetCardIcon: '#6461D2',
-      worksheetBoxText: '#333333',
-      alertBackground: '#B9FBC0',
-      worksheetText: '#373839',
-      worksheetCloseIcon: '#AA948E',
-      messageInfo: '#7C7E82'
-    },    
+    }
   },
   fonts: {
     heading: fontFamily,
