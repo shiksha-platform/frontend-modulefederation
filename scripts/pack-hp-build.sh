2953eac31fc8c3b37cf0a1d7a5b79ee4a03a67ec
snehal patil <snehalpatil0904@gmail.com>
2022-10-19 13:42:52 +0530

    Update pack-hp-build.sh

--- scripts/pack-hp-build.sh
+++ scripts/pack-hp-build.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
-# update the API url in environment file 
-
-rm -rf hp-build
-mkdir hp-build
-mkdir hp-build/modules
-find packages \
-\( \
- -path packages/attendance \
--o -path packages/core \) \
--type d  -maxdepth 1 -mindepth 1 -exec bash -c '
+rm -rf prod-build
+mkdir prod-build
+mkdir prod-build/modules
+find packages \( ! -path packages/common-lib -o ! -path packages/teacher-app \) -type d  -maxdepth 1 -mindepth 1 -exec bash -c '
 for f  do
     # echo $f
     if [ $f != "packages/common-lib" ] &&  [ $f != "packages/teacher-app" ] ; then
         echo "Processing ${f//packages\//}"
-        cp -rf "$f/build" "hp-build/modules/${f//packages\//}"
+        cp -rf "$f/build" "prod-build/modules/${f//packages\//}"
     fi
 done 
 ' sh {} +
-# do not copy teachers app
-cp -r  packages/teacher-app/build/* hp-build/
-find  hp-build -name  'modules.json' | xargs sed -i 's|http://localhost:[0-9]*||g'
-cd hp-build && tar -cf ../shiksha-hp-ui.tar . && cd ../
+cp -r  packages/teacher-app/build/* prod-build/
+find  prod-build -name  'modules.json' | xargs sed -i 's|http://localhost:[0-9]*||g'
+cd prod-build && tar -cf ../shiksha-ui.tar . && cd ../
